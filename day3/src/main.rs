ee485f76d8b9901bec0a07fffda32c0296050fb1
Luis Nogoseke <luis.nogoseke@venkonetworks.com>
2024-12-03 18:06:20 -0300

    Day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,8 +5,8 @@ use std::path::Path;
 
 fn main() {
     if let Ok(lines) = read_lines("input.txt") {
-        part1(lines);
-        //        part2(lines);
+        //  part1(lines);
+        part2(lines);
     }
 }
 
@@ -28,8 +28,33 @@ fn part1(lines: std::io::Lines<BufReader<File>>) {
     println!("Result: {result}");
 }
 
-//fn part2(lines: std::io::Lines<BufReader<File>>)  {
-//}
+fn part2(lines: std::io::Lines<BufReader<File>>) {
+    let mut result = 0;
+    let mut enabled = true;
+    let re = Regex::new(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|(don't\(\))|(do\(\))").unwrap();
+    let operands = Regex::new(r"[0-9]{1,3}").unwrap();
+    for line in lines.flatten() {
+        let muls: Vec<&str> = re.find_iter(&line).map(|m| m.as_str()).collect();
+
+        for input in muls {
+            if input == "don't()" {
+                enabled = false;
+                continue;
+            } else if input == "do()" {
+                enabled = true;
+                continue;
+            }
+            let ops: Vec<i32> = operands
+                .find_iter(input)
+                .map(|m| m.as_str().parse::<i32>().unwrap())
+                .collect();
+            if enabled && ops.len() == 2 {
+                result += ops[0] * ops[1];
+            }
+        }
+    }
+    println!("Result: {result}");
+}
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
