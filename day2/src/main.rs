31b04300726bf14f0443526c96673c16d7e71007
Luis Nogoseke <luis.nogoseke@venkonetworks.com>
2024-12-03 16:39:20 -0300

    Day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,15 +5,15 @@ use std::path::Path;
 
 fn main() {
 if let Ok(lines) = read_lines("input.txt") {
-        part1(lines);
-//        part2(lines);
+//        part1(lines);
+        part2(lines);
     }
 
 }
 
 fn part1(lines: std::io::Lines<BufReader<File>>)  {
         let mut result = 0;
-        let mut n_line = 0;
+        //let mut n_line = 0;
         for line in lines.flatten() {
            // println!("Line {n_line}");
            let split: Vec<i32> = line.split(' ')
@@ -47,13 +47,61 @@ fn part1(lines: std::io::Lines<BufReader<File>>)  {
             }
             temp = i;
         }
-        n_line += 1;
+        //n_line += 1;
     }
         println!("Result: {result}");
 }
 
-//fn part2(lines: std::io::Lines<BufReader<File>>)  {
-//}
+fn part2(lines: std::io::Lines<BufReader<File>>)  {
+        let mut result = 0;
+        for line in lines.flatten() {
+           let split: Vec<i32> = line.split(' ')
+                .map(|d| d.parse::<i32>().unwrap())
+                .collect();
+            let length = split.len();
+            if is_valid(&split) {
+                result += 1;
+                continue;
+            }
+            for i in 0..length {
+                let mut temp_report = split.clone();
+                temp_report.remove(i);
+                if is_valid(&temp_report) {
+                    result += 1;
+                    break;
+                }
+            }
+        }
+        println!("Result {result}");
+}
+
+fn is_valid(report: &Vec<i32>) -> bool {
+    let mut first = true;
+    let mut temp = 0;
+    let ascending = report[1] > report[2];
+    for i in report {
+        if first {
+            first = false;
+            temp = *i;
+            continue;
+        }
+        if ascending && (*i > temp){
+            // println!("Ascending and failed {i}  {temp}");
+            return false;
+        }
+        if !ascending && (*i < temp) {
+            // println!("Descending and failed");
+            return false;
+
+        }
+        if (*i - temp).abs() < 1 || (*i -temp).abs() > 3 {
+            // println!("Bigger step than allowed {i}  {temp}");
+            return false;
+        }
+        temp = *i;
+    }
+    true
+}
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
